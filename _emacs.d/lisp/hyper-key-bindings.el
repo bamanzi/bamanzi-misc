8d446c5a54ad29fcc735850c2d7bebf9b824d2c1
bamanzi@gmail.com <bamanzi@gmail.com@5d397f6c-e14f-10ea-c86a-4de507d69060>
2011-03-20 12:02:16 +0000

    * Changed mapping of <apps> from 'super to 'hyper modifier, to commendate to Linux's default settings * Added H-j, H-* & H-# (powered by highlight-symbol)

--- _emacs.d/lisp/hyper-key-bindings.el
+++ _emacs.d/lisp/hyper-key-bindings.el
@@ -1,101 +1,170 @@
-(require 'undo-tree)
-;;(require 'redo)
-(require 'bm)
-;;(require 'auto-complete)
+;;; hyper-key-bindings.el -- Some key bindings that use 'hyper as modifier
+
+
+;; initial magics
 
 (if (memq (framep (selected-frame)) '(w32 win32))
     (progn
       ;;(setq w32-pass-apps-to-system nil)
-      (setq w32-apps-modifier 'super)
+      (setq w32-apps-modifier 'hyper)
 
-      (setq w32-pass-lwindow-to-system nil)
-      (setq w32-lwindow-modifier 'super)))
+      (setq w32-phantom-key-code 65)  ;;It seems no use
+      (setq w32-pass-lwindow-to-system nil) ;; Prevent single press of <lwindow> to open Windows Start Menu
+                                      ;; but could not prevent Win+R, Win+L etc 
 
-;; on nowaday linux, Windows key is Super
+      ;(setq w32-lwindow-modifier 'super)
+      ;(setq w32-pass-rwindow-to-system nil) ;; It semmes no use
+      ;(setq w32-rwindow-modifier 'super)
 
-;; editing
-(global-set-key (kbd "s-c") 'kill-ring-save) ;; copy
-(global-set-key (kbd "s-x") 'kill-region) ;; cut
-(global-set-key (kbd "s-v") 'yank)	;; paste
+      ;;(setq w32-capslock-is-shiftlock t)   ;;useful?
+      ;;(setq w32-scroll-lock-modifier 'alt) ;;useful?
+      ))  ;;FIXME: what about Linux?
 
-(global-set-key (kbd "s-a") 'mark-whole-buffer)  ;; select all
+;;{{{ features
+;;you can comment out the features/libraries you don't want to enable
+
+;;(global-semantic-mode t)
+
+;;3rd-party libraries
+
+(if (load "undo-tree" t)
+    (global-undo-tree-mode t)
+  (load "redo"))
+
+(load "bm" t)			;; visual bookmarks (within buffer)
+;;(require 'auto-complete)
+
+(load "highlight-symbol" t)
+(load "anything" t)
+(load "anything-config" t)	;; for anything-recentf & anything-imenu
+(load "idomenu" t)		;; imenu navigation within minibuffer
+(load "eassist" t)		;; CEDET's eassist (for eassist-switch-h-cpp, eassist-list-methods )
+
+;;}}}
+
+
+;;{{{ CUA-like editing
+(global-set-key (kbd "H-c") 'kill-ring-save) ;; copy
+(global-set-key (kbd "H-x") 'kill-region) ;; cut
+(global-set-key (kbd "H-v") 'yank)	;; paste
+
+(global-set-key (kbd "H-a") 'mark-whole-buffer)  ;; select all
 
 (if (featurep 'undo-tree)
     (progn
-      (global-set-key (kbd "s-z") 'undo-tree-undo)
-      (global-set-key (kbd "s-S-z") 'undo-tree-redo)
-      (global-set-key (kbd "s-y") 'undo-tree-redo))
+      (global-set-key (kbd "H-z") 'undo-tree-undo)
+      (global-set-key (kbd "H-Z") 'undo-tree-redo)
+      (global-set-key (kbd "H-y") 'undo-tree-redo))
   (progn
-    (global-set-key (kbd "s-z") 'undo)
+    (global-set-key (kbd "H-z") 'undo)
     (if (featurep 'redo)
 	(progn
-	  (global-set-key (kbd "s-S-z") 'redo)
-	  (global-set-key (kbd "s-y") 'redo)))))
+	  (global-set-key (kbd "H-Z") 'redo)
+	  (global-set-key (kbd "H-y") 'redo)))))
 
 
-(global-set-key (kbd "s-w") 'toggle-truncate-lines) ;; or visual-line-mode?
-
+(global-set-key (kbd "H-w") 'toggle-truncate-lines) ;; or visual-line-mode?
 
 ;; files
-;;(global-set-key (kbd "s-n") 'create-empty-buffer) ;;FIXME
-(global-set-key (kbd "s-o") 'menu-find-file-existing)
-(global-set-key (kbd "s-s") 'save-buffer)
-(global-set-key (kbd "s-S-s") 'write-file)
+;;(global-set-key (kbd "H-n") 'create-empty-buffer) ;;FIXME
+;;(global-set-key (kbd "H-o") 'menu-find-file-existing)
+(global-set-key (kbd "H-s") 'save-buffer)
+(global-set-key (kbd "H-S") 'write-file)
 
 ;; search
-(global-set-key (kbd "s-f") 'isearch-forward)
-(global-set-key (kbd "s-h") 'query-replace)
-(global-set-key (kbd "<s-f3>") 'nonincremental-repeat-search-forward)
-(global-set-key (kbd "<C-s-f3>") 'nonincremental-repeat-search-backward)
-
-;; bookmarks (within buffer)
-(if (featurep 'bm)
-    (progn
-      (global-set-key (kbd "<s-f2>") 'bm-next)
-      (global-set-key (kbd "<C-s-f2>") 'bm-toggle)
-      (global-set-key (kbd "<S-s-f2>") 'bm-previous)))
-
-;;; misc
-(global-set-key (kbd "s-l") '(lambda()
-			       (interactive)
-			       (occur (format "%s" (thing-at-point 'symbol)))))
-
-(global-set-key (kbd "s-;") 'comment-or-uncomment-region)
-
-;; completion  (Emacs default: M-TAB - lisp-complete-symbol, M-/ - dabbrev-expand)
-(global-set-key (kbd "s-/") 'hippie-expand)
+(global-set-key (kbd "H-f") 'isearch-forward)
+(global-set-key (kbd "H-h") 'query-replace)
+(global-set-key (kbd "<H-f3>") 'nonincremental-repeat-search-forward)
+(global-set-key (kbd "<C-S-f3>") 'nonincremental-repeat-search-backward)
+;;}}}
 
 
-;; programming
-(load "eassist" t)
-(load "idomenu" t)
+;;{{{ programming
+
+(global-set-key (kbd "H-;") 'comment-or-uncomment-region)
+
 (defun hkb-list-methods()
   (interactive)
-  (if (and (boundp 'eassist-list-methods)
+  (cond
+   ( (fboundp 'anything-imenu) (anything-imenu) )
+   ( (fboundp 'idomenu) (idomenu) )
+   ( (and (fboundp 'eassist-list-methods)
 	   (memq major-mode '(emacs-lisp-mode c-mode java-mode python-mode))
 	   (memq 'semantic-mode minor-mode-alist))
-      (eassist-list-methods)
-    (idomenu)))
-(global-set-key (kbd "s-[") 'hkb-list-methods)
+     (eassist-list-methods) )
+   (t (imenu))))
+(global-set-key (kbd "H-o") 'hkb-list-methods)
 
 (defun hkb-goto-symbol()
   (interactive)
-  (let ((keyword (symbol-value (thing-at-point 'symbol))))
+  (let ( (keyword (thing-at-point 'symbol)) )
     (cond
-     ((eq major-mode emacs-lisp-mode)
-      (find-function-at-point))
-     ((memq major-mode '(c-mode java-mode python-mode))
-      (semantic-complete-jump))
+     ( (eq major-mode 'emacs-lisp-mode)
+       (find-function-at-point))
+     ( (and (fboundp 'semantic-complete-jump)
+	     (memq major-mode '(c-mode java-mode python-mode)))
+       (semantic-complete-jump))
      (t
       (find-tag)))))
-(global-set-key (kbd "s-]") 'hkb-goto-symbol)
+(global-set-key (kbd "H-]") 'hkb-goto-symbol)
 
-(if (featurep 'tabbar)
+(if (featurep 'eassist)
+    (global-set-key (kbd "H-`") 'eassist-switch-h-cpp))
+;;}}}
+
+;;{{{ operation on current word/symbol
+(defun hkb-goto-symbol-occurrence (forward)
+  (let ( (symbol (thing-at-point 'symbol)) )
+    (if (not (member symbol highlight-symbol-list))
+	(highlight-symbol-at-point))  ;;FIXME
+    (if forward
+	(highlight-symbol-next)
+      (highlight-symbol-prev))))
+
+(defun hkb-goto-symbol-next-occur ()
+  (interactive)
+  (hkb-goto-symbol-occurrence t))
+
+(defun hkb-goto-symbol-prev-occur ()
+  (interactive)
+  (hkb-goto-symbol-occurrence nil))
+      
+
+(if (featurep 'highlight-symbol)
     (progn
-      (global-set-key (kbd "<s-tab>") 'tabbar-forward-tab)
-      (global-set-key (kbd "<M-s-tab>") 'tabbar-backward-tab)))
+      (global-set-key (kbd "H-j") 'highlight-symbol-at-point)
+      (global-set-key (kbd "H-*") 'hkb-goto-symbol-next-occur)
+      (global-set-key (kbd "H-#") 'hkb-goto-symbol-prev-occur)))
+      
+;; occur
 
-;;; misc
+(global-set-key (kbd "H-l") '(lambda()
+			       (interactive)
+			       (occur (format "%s" (thing-at-point 'symbol)))))
+
+;; grep
+
+;; completion  (Emacs default: M-TAB - lisp-complete-symbol, M-/ - dabbrev-expand)
+;;(global-set-key (kbd "H-/") 'hippie-expand)
+
+;;auto-complete?
+
+;;stardict
+
+
+;;}}}
+
+
+;;{{{ misc
+;; bookmarks (within buffer)
+(if (featurep 'bm)
+    (progn
+      (global-set-key (kbd "<H-f2>") 'bm-next)
+      (global-set-key (kbd "<H-C-f2>") 'bm-toggle)
+      (global-set-key (kbd "<H-S-f2>") 'bm-previous)))
+
+
+
 ;;; select rectangle using H-mouse-1
 (require 'cua-rect)
 (defun hkb-mouse-mark-cua-rectangle (event)
@@ -103,8 +172,8 @@
   (if (not cua--rectangle)
       (cua-mouse-set-rectangle-mark event)
     (cua-mouse-resize-rectangle event)))
-(global-set-key (kbd "<s-mouse-1>") 'hkb-mouse-mark-cua-rectangle)
-(define-key cua--rectangle-keymap (kbd "<s-mouse-1>") 'hkb-mouse-mark-cua-rectangle)
-    
+(global-set-key (kbd "<H-mouse-1>") 'hkb-mouse-mark-cua-rectangle)
+(define-key cua--rectangle-keymap (kbd "<H-mouse-1>") 'hkb-mouse-mark-cua-rectangle)
 
+;;}}}
 
