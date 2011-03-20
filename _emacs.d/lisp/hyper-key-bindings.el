19dd411623e3289f6c1a980cac791e64e9298aee
bamanzi@gmail.com <bamanzi@gmail.com@5d397f6c-e14f-10ea-c86a-4de507d69060>
2011-03-20 14:18:23 +0000

    Added some window commands to super-modifier-based keys. Added super/hyper configuration for Linux-X/MacOS

--- _emacs.d/lisp/hyper-key-bindings.el
+++ _emacs.d/lisp/hyper-key-bindings.el
@@ -1,29 +1,70 @@
-;;; hyper-key-bindings.el -- Some key bindings that use 'hyper as modifier
+;;; hyper-key-bindings.el -- Some functions bind to 'hyper-, 'super- keys
 
+;; <lwindows>, <rwindows>, <menu>, <capslock> 
 
-;; initial magics
+;;{{{ initial magics
+(defun enable-hyper-super-modifiers-win32 ()
+       ;;(setq w32-pass-apps-to-system nil)
+       (setq w32-apps-modifier 'hyper)
 
-(if (memq (framep (selected-frame)) '(w32 win32))
-    (progn
-      ;;(setq w32-pass-apps-to-system nil)
-      (setq w32-apps-modifier 'hyper)
+       (setq w32-pass-lwindow-to-system nil)
+       (setq w32-lwindow-modifier 'super)
 
-      (setq w32-phantom-key-code 65)  ;;It seems no use
-      (setq w32-pass-lwindow-to-system nil) ;; Prevent single press of <lwindow> to open Windows Start Menu
-                                      ;; but could not prevent Win+R, Win+L etc 
+       ;;(setq w32-capslock-as-
+       )
 
-      ;(setq w32-lwindow-modifier 'super)
-      ;(setq w32-pass-rwindow-to-system nil) ;; It semmes no use
-      ;(setq w32-rwindow-modifier 'super)
+(defun enable-hyper-super-modifiers-linux-x ()
+  ;; on nowaday linux, <Windows> key is usually configured to Super 
+    
+  ;; menu key as hyper (Note: for H-s, you need to release <menu> key before pressing 's')
+  (define-key key-translation-map [menu] 'event-apply-hyper-modifier) ;H-
+  (define-key key-translation-map [apps] 'event-apply-hyper-modifier)
 
-      ;;(setq w32-capslock-is-shiftlock t)   ;;useful?
-      ;;(setq w32-scroll-lock-modifier 'alt) ;;useful?
-      ))  ;;FIXME: what about Linux?
+  ;; by default, Emacs bind <menu> to execute-extended-command (same as M-x)
+  ;; now <menu> defined as 'hyper, we need to press <menu> twice to get <H-menu>
+  (global-set-key (kbd "<H-menu>") 'execute-extended-command)
+  )
+
+(defun enable-hyper-super-modifiers-linux-console ()
+  (message "fixme: enable-hyper-super-modifiers-linux-console"))
+
+(defun enable-hyper-super-modifiers-macos ()
+  ;; http://xahlee.org/emacs/emacs_hyper_super_keys.html
+  (setq mac-option-modifier 'hyper) ; sets the Option key as Hyper
+  (setq mac-option-modifier 'super) ; sets the Option key as Super
+  (setq mac-command-modifier 'meta) ; sets the Command key as Meta
+  (setq mac-control-modifier 'meta) ; sets the Control key as Meta
+  )
+
+(defun enable-hyper-super-modifiers ()
+  (let ( (frame (framep (selected-frame))) )
+    (cond
+     ( (memq frame '(w32 win32))
+       (enable-hyper-super-modifiers-win32) )
+     ( (eq frame 'x)
+       (enable-hyper-super-modifiers-linux-x ) )
+     ( (eq frame 'ns)
+       (enable-hyper-super-modifiers-macos) )
+     ( frame
+       (enable-hyper-super-modifiers-linux-console ))
+     ( t
+       (message "fixmed: enable-hyper-super-modifiers") )
+    ))
+
+  ;; you can always use "C-c h" as 'hyper modifier, even in Linux console or DOS
+  (define-key key-translation-map (kbd "C-c h") 'event-apply-hyper-modifier)
+  (define-key key-translation-map (kbd "C-c s") 'event-apply-super-modifier)
+  (define-key key-translation-map (kbd "C-c a") 'event-apply-alt-modifier)
+  )
+
+(enable-hyper-super-modifiers)
+;;}}}
 
 ;;{{{ features
 ;;you can comment out the features/libraries you don't want to enable
 
 ;;(global-semantic-mode t)
+
 
 ;;3rd-party libraries
 
@@ -39,6 +80,10 @@
 (load "anything-config" t)	;; for anything-recentf & anything-imenu
 (load "idomenu" t)		;; imenu navigation within minibuffer
 (load "eassist" t)		;; CEDET's eassist (for eassist-switch-h-cpp, eassist-list-methods )
+
+(load "folding" t)		;; complementary for outline/hideshow, user-definable mark-based folding
+(load "fold-dwim" t)		;; uniformed front-end for outline/hideshow/folding
+
 
 ;;}}}
 
@@ -108,8 +153,6 @@
       (find-tag)))))
 (global-set-key (kbd "H-]") 'hkb-goto-symbol)
 
-(if (featurep 'eassist)
-    (global-set-key (kbd "H-`") 'eassist-switch-h-cpp))
 ;;}}}
 
 ;;{{{ operation on current word/symbol
@@ -145,6 +188,10 @@
 ;; grep
 
 ;; completion  (Emacs default: M-TAB - lisp-complete-symbol, M-/ - dabbrev-expand)
+
+;;(if (< emacs-major-version 24)
+;;    (global-set-key (kbd "H-/") 'lisp-complete-symbol)))
+(global-set-key (kbd "H-/") 'completion-at-point)
 ;;(global-set-key (kbd "H-/") 'hippie-expand)
 
 ;;auto-complete?
@@ -154,6 +201,67 @@
 
 ;;}}}
 
+;;{{{ windows
+(global-set-key (kbd "<s-up>") 'enlarge-window)
+(global-set-key (kbd "<s-down>") 'shrink-window)
+(global-set-key (kbd "<s-left>") 'shrink-window-horizontally)
+(global-set-key (kbd "<s-right>") 'enlarge-window-horizontally)
+
+(global-set-key (kbd "<S-s-up>") '(lambda ()
+				   (interactive)
+				   (enlarge-window 3)))
+(global-set-key (kbd "<S-s-down>") '(lambda ()
+				      (interactive)
+				      (shrink-window 3)))
+(global-set-key (kbd "<S-s-left>") '(lambda ()
+				      (interactive)
+				      (shrink-window-horizontally 5)))
+(global-set-key (kbd "<S-s-right>") '(lambda ()
+				       (interactive)
+				       (enlarge-window-horizontally 5)))
+
+(global-set-key (kbd "<s-=>") '(lambda ()
+				 (interactive)
+				 (enlarge-window 1)
+				 (enlarge-window-horizontally 1)))
+(global-set-key (kbd "<s-->") '(lambda ()
+				 (interactive)
+				 (shrink-window 1)
+				 (shrink-window-horizontally 1)))
+(global-set-key (kbd "<s-+>") '(lambda ()
+				 (interactive)
+				 (enlarge-window 3)
+				 (enlarge-window-horizontally 5)))
+(global-set-key (kbd "<s-_>") '(lambda ()
+				 (interactive)
+				 (shrink-window 3)
+				 (shrink-window-horizontally 5)))
+
+
+;; https://github.com/banister/window-rotate-for-emacs
+(defun rotate-windows-helper(x d)
+  (if (equal (cdr x) nil) (set-window-buffer (car x) d)
+    (set-window-buffer (car x) (window-buffer (cadr x))) (rotate-windows-helper (cdr x) d)))
+ 
+(defun rotate-windows ()
+  (interactive)
+  (rotate-windows-helper (window-list) (window-buffer (car (window-list))))
+  (select-window (car (last (window-list)))))
+
+(global-set-key (kbd "<s-backspace>") 'rotate-windows)
+;;TODO: backward-rotate-windows
+
+	     
+(global-set-key (kbd "<s-tab>") 'other-window)
+(global-set-key (kbd "<S-s-tab>") '(lambda ()
+				     (interactive)
+				     (other-window -1)))
+;; (if (featurep 'tabbar)
+;;     (progn
+;;       (global-set-key (kbd "<C-tab>")   'tabbar-forward-tab)
+;;       (global-set-key (kbd "<C-s-tab>") 'tabbar-backward-tab)))
+
+;;}}}
 
 ;;{{{ misc
 ;; bookmarks (within buffer)
@@ -163,17 +271,21 @@
       (global-set-key (kbd "<H-C-f2>") 'bm-toggle)
       (global-set-key (kbd "<H-S-f2>") 'bm-previous)))
 
+;; folding
+
+(global-set-key (kbd "H-=") 'fold-dwim-toggle)
+(global-set-key (kbd "H-+") 'fold-dwim-show-all)
+(global-set-key (kbd "H-_") 'fold-dwim-hide-all)
 
 
 ;;; select rectangle using H-mouse-1
-(require 'cua-rect)
+(require 'cua-rect)	
 (defun hkb-mouse-mark-cua-rectangle (event)
   (interactive "e")
-  (if (not cua--rectangle)
+  (if (not cua--rectangle)	  
       (cua-mouse-set-rectangle-mark event)
     (cua-mouse-resize-rectangle event)))
-(global-set-key (kbd "<H-mouse-1>") 'hkb-mouse-mark-cua-rectangle)
-(define-key cua--rectangle-keymap (kbd "<H-mouse-1>") 'hkb-mouse-mark-cua-rectangle)
-
-;;}}}
+(global-set-key (kbd "<s-mouse-1>") 'hkb-mouse-mark-cua-rectangle)
+(define-key cua--rectangle-keymap (kbd "<s-mouse-1>") 'hkb-mouse-mark-cua-rectangle)
+    
 
