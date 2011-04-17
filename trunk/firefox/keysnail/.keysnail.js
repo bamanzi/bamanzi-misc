0d54c8fe730495acde493ede1c6b8383a8a088b1
bamanzi@gmail.com <bamanzi@gmail.com@5d397f6c-e14f-10ea-c86a-4de507d69060>
2011-04-17 12:18:27 +0000

    1. tidy the code for Fox Splitter (move to PRESERVE area) 2. add 'next-page', 'previouse-page', 3. add 'search-selection', 'paste-and-go', 'paste-to-tab-and-go'

--- trunk/firefox/keysnail/.keysnail.js
+++ trunk/firefox/keysnail/.keysnail.js
@@ -5,6 +5,153 @@
 // ========================================================================= //
 //{{%PRESERVE%
 // Put your codes here
+
+
+//Use Fox Splitter addon to mimic C-x 1/2/3 stuff
+ext.add("other-window", function() {
+    function focusSubBrowserById(aId) {
+        SplitBrowser.getSubBrowserById(aId).browser.contentWindow.focus();
+    }
+
+    var browsers = SplitBrowser.browsers;
+    if (SplitBrowser.activeBrowser === gBrowser) {
+        focusSubBrowserById(browsers[arg == null ? 0 : browsers.length - 1].id);
+        return;
+    }
+    var id = SplitBrowser.activeSubBrowser.id;
+    for (var i = 0; i < browsers.length; i++) {
+        if (browsers[i].id == id) {
+            break;
+        }
+    }
+    var nextIndex = arg == null ? i + 1 : i - 1;
+    if (nextIndex >= browsers.length || nextIndex < 0) {
+        gBrowser.contentWindow.focus();
+    } else {
+        focusSubBrowserById(browsers[nextIndex].id);
+    }
+}, 'Select another window (Fox Splitter addon)');
+
+ext.add("delete-frame", function() {
+    SplitBrowser.activeBrowserCloseWindow();
+}, 'Close current active "window" (SplitBrowser addon)');
+
+ext.add("delete-window", function() {
+    if (typeof SplitBrowser == "undefined") {
+        BrowserCloseTabOrWindow();
+    } else {
+        var b = SplitBrowser.activeBrowser;
+        if (b.mTabs.length > 1) {
+            b.removeTab(b.mCurrentTab);
+        } else if (b === gBrowser) {
+            gBrowser.removeTab(gBrowser.mCurrentTab);
+        }
+    }
+}, 'Close tab (or \'window\', if Spli tBrowser addon installed)');
+
+ext.add("delete-other-windows", function() {
+    var url = SplitBrowser.activeBrowser != gBrowser ? SplitBrowser.activeSubBrowser.src : null;
+    var browsers = SplitBrowser.browsers;
+    for (var i = 0; i < browsers.length; ++i) {
+        browsers[i].close();
+    }
+    if (url) {
+        window.loadURI(url);
+    }
+}, 'delete-other-window (Fox Splitter addon)');
+
+
+ext.add("split-window-vertically" , function() {
+    SplitBrowser.addSubBrowser(window.content.location.href, SplitBrowser.activeSubBrowser, SplitBrowser.POSITION_BOTTOM);
+}, 'split-window-vertically (Fox Splitter addon)');
+
+ext.add("split-window-horizontally", function() {
+    SplitBrowser.addSubBrowser(window.content.location.href, SplitBrowser.activeSubBrowser, SplitBrowser.POSITION_RIGHT);
+}, 'split-window-horizontally (Fox Splitter addon)');
+
+
+
+//Yet Another Twitter Client KeySnail 
+plugins.options["twitter_client.keymap"] = {
+    "C-z"   : "prompt-toggle-edit-mode",
+    "SPC"   : "prompt-next-page",
+    "b"     : "prompt-previous-page",
+    "j"     : "prompt-next-completion",
+    "k"     : "prompt-previous-completion",
+    "g"     : "prompt-beginning-of-candidates",
+    "G"     : "prompt-end-of-candidates",
+    "q"     : "prompt-cancel",
+    // twitter client specific actions
+    "t"     : "tweet",
+    "r"     : "reply",
+    "R"     : "retweet",
+    "D"     : "delete-tweet",
+    "f"     : "add-to-favorite",
+    "v"     : "display-entire-message",
+    "V"     : "view-in-twitter",
+    "c"     : "copy-tweet",
+    "s"     : "show-target-status",
+    "@"     : "show-mentions",
+    "/"     : "search-word",
+    "o"     : "open-url"
+};
+
+plugins.options["twitter_client.update_interval"] = 10000;
+
+
+//misc stuff
+ext.add("previous-page", function () {
+    var document = window._content.document;
+    var links = document.links;
+    for(i = 0; i < links.length; i++) {
+        if (   (links[i].text == '上一页')   || (links[i].text == '<上一页') 
+            || (links[i].text == 'Previous') || (links[i].text == 'Prev') 
+            || (links[i].text == '<')        || (links[i].text == '<<')) 
+        document.location = links[i].href;
+    }
+}, "Previous page");
+
+ext.add("next-page", function () {
+    var document = window._content.document;
+    var links = document.links;
+    for(i = 0; i < links.length; i++) {
+    if (   (links[i].text == '下一页')  || (links[i].text == '下一页>') 
+        || (links[i].text == 'Next')    || (links[i].text == 'next') 
+        || (links[i].text == '>')       || (links[i].text == '>>')) 
+        document.location = links[i].href;
+    }
+}, "Next page");
+
+
+ext.add("search-selection", function() {
+    if(!getBrowserSelection()) return;
+    BrowserSearch.loadSearch(getBrowserSelection(), true);
+}, "Use the default search engine to search the phrase currently selected");
+
+ext.add("paste-and-go", function() {
+    var url = command.getClipboardText();
+    if (url.indexOf("://") != -1)
+    {
+	window._content.location = url;
+    }
+    else
+    {
+	//url = util.format("http://www.google.com/search?q=%s&ie=utf-8&oe=utf-8", encodeURIComponent(url));
+	BrowserSearch.loadSearch(url, false);
+    }
+}, "Paste the URL or keyword from clipboard and Go");
+
+ext.add("paste-to-tab-and-go", function() {
+    var url = command.getClipboardText();
+    if (url.indexOf("://") != -1)
+	gBrowser.loadOneTab(url, null, null, null, false);
+    else
+    {
+	//url = util.format("http://www.google.com/search?q=%s&ie=utf-8&oe=utf-8", encodeURIComponent(url));
+	BrowserSearch.loadSearch(url, true);
+    }
+}, "Paste the URL or keyword from clipboard to a new tab and Go");
+
 //}}%PRESERVE%
 // ========================================================================= //
 
@@ -136,64 +283,27 @@ key.setGlobalKey(['C-x', 's'], function (ev) {
 }, 'Focus to the first button', true);
 
 key.setGlobalKey(['C-x', '0'], function (ev, arg) {
-    SplitBrowser.activeBrowserCloseWindow();
+    ext.exec("delete-window", arg, ev);
 }, 'Close current active "window" (Fox Splitter addon)');
 
 key.setGlobalKey(['C-x', '1'], function (ev, arg) {
-    var url = SplitBrowser.activeBrowser != gBrowser ? SplitBrowser.activeSubBrowser.src : null;
-    var browsers = SplitBrowser.browsers;
-    for (var i = 0; i < browsers.length; ++i) {
-        browsers[i].close();
-    }
-    if (url) {
-        window.loadURI(url);
-    }
+    ext.exec("delete-other-windows", arg, ev);
 }, 'close-other-window (Fox Splitter addon)');
 
 key.setGlobalKey(['C-x', '2'], function (ev, arg) {
-    SplitBrowser.addSubBrowser(window.content.location.href, SplitBrowser.activeSubBrowser, SplitBrowser.POSITION_BOTTOM);
+    ext.exec("split-window-vertically", arg, ev);
 }, 'split-window-vertically (Fox Splitter addon)');
 
 key.setGlobalKey(['C-x', '3'], function (ev, arg) {
-    SplitBrowser.addSubBrowser(window.content.location.href, SplitBrowser.activeSubBrowser, SplitBrowser.POSITION_RIGHT);
+    ext.exec("split-window-horizontally", arg, ev);
 }, 'split-window-horizontally (Fox Splitter addon)');
 
 key.setGlobalKey(['C-x', 'k'], function (ev) {
-    if (typeof SplitBrowser == "undefined") {
-        BrowserCloseTabOrWindow();
-    } else {
-        var b = SplitBrowser.activeBrowser;
-        if (b.mTabs.length > 1) {
-            b.removeTab(b.mCurrentTab);
-        } else if (b === gBrowser) {
-            gBrowser.removeTab(gBrowser.mCurrentTab);
-        }
-    }
-}, 'Close tab (or \'window\', if SplitBrowser addon installed)');
+    ext.exec("delete-window", arg, ev);
+}, 'Close tab (or \'window\', if Fox Splitter addon installed)');
 
 key.setGlobalKey(['C-x', 'o'], function (ev, arg) {
-
-    function focusSubBrowserById(aId) {
-        SplitBrowser.getSubBrowserById(aId).browser.contentWindow.focus();
-    }
-
-    var browsers = SplitBrowser.browsers;
-    if (SplitBrowser.activeBrowser === gBrowser) {
-        focusSubBrowserById(browsers[arg == null ? 0 : browsers.length - 1].id);
-        return;
-    }
-    var id = SplitBrowser.activeSubBrowser.id;
-    for (var i = 0; i < browsers.length; i++) {
-        if (browsers[i].id == id) {
-            break;
-        }
-    }
-    var nextIndex = arg == null ? i + 1 : i - 1;
-    if (nextIndex >= browsers.length || nextIndex < 0) {
-        gBrowser.contentWindow.focus();
-    } else {
-        focusSubBrowserById(browsers[nextIndex].id);
-    }
+    ext.exec("other-window", arg, ev);
 }, 'other-window (Fox Splitter addon)', true);
 
 key.setGlobalKey([['C-x', 'K'], ['C-x', '5', '0']], function (ev) {
@@ -300,19 +410,19 @@ key.setViewKey(['C-c', 'l'], function (ev) {
     BrowserForward();
 }, 'Forward');
 
-key.setViewKey(['C-c', 'f'], function (ev, arg) {
+key.setGlobalKey(['C-c', 'f'], function (ev, arg) {
     ext.exec("hok-start-foreground-mode", arg, ev);
 }, 'Start Hit a Hint foreground mode', true);
 
-key.setViewKey(['C-c', 'F'], function (ev, arg) {
+key.setGlobalKey(['C-c', 'F'], function (ev, arg) {
     ext.exec("hok-start-background-mode", arg, ev);
 }, 'Start Hit a Hint background mode', true);
 
-key.setViewKey(['C-c', ';'], function (ev, arg) {
+key.setGlobalKey(['C-c', ';'], function (ev, arg) {
     ext.exec("hok-start-extended-mode", arg, ev);
 }, 'Start Hit a Hint extended mode', true);
 
-key.setViewKey(['C-c', 'C-e'], function (ev, arg) {
+key.setGlobalKey(['C-c', 'C-e'], function (ev, arg) {
     ext.exec("hok-start-continuous-mode", arg, ev);
 }, 'Start Hit a Hint continuous mode', true);
 
@@ -327,7 +437,32 @@ key.setViewKey(['C-c', 'i'], function (ev, arg) {
 key.setViewKey(['C-c', ':'], function (ev, arg) {
     shell.input(null, arg);
 }, 'List and execute commands', true);
+    
+key.setGlobalKey(['C-o', 'o'], function (ev, arg) {
+    shell.input("Open ", arg);
+}, 'Open an URL', true);
+    
+key.setGlobalKey(['C-o', 't'], function (ev, arg) {
+    shell.input("tabopen ", arg);
+}, 'Open an URL in new tab', true);
+    
+key.setGlobalKey(['C-o', 'f'], function (ev, arg) {
+		     BrowserOpenFileWindow();
+}, 'Open an existing file', true);
+    
+key.setGlobalKey(['C-o', 'b'], function (ev, arg) {
+		 ext.exec("bmany-list-all-bookmarks", arg, ev);
+}, 'bmany - List all bookmarks', true);
+    
+    key.setViewKey(['C-o', 'B'], function (ev, arg) {
+    ext.exec("bmany-list-bookmarklets", arg, ev);
+}, "bmany - List all bookmarklets");
+	
+    key.setGlobalKey(['C-o', 'k'], function (ev, arg) {
+		 ext.exec("bmany-list-all-bookmarks-with-keyword", arg, ev);
+}, 'bmany - List bookmarks with keyword', true);
 
+    
 key.setViewKey(['C-c', 'd'], function (ev) {
     BrowserCloseTabOrWindow();
 }, 'Close tab / window');
@@ -649,4 +784,50 @@ key.setGlobalKey(['C-c', 'g', 'U'], function (ev) {
         loadURI(root, null, null);
     }
 }, 'Go to the root directory', true);
+    
+    
+key.setGlobalKey(["C-c", "t", "t"],
+    function (ev, arg) {
+        ext.exec("twitter-client-tweet", arg);
+    }, "Tweet", true);
+
+key.setGlobalKey(["C-c", "t", "T"],
+    function (ev, arg) {
+        ext.exec("twitter-client-tweet-this-page", arg);
+    }, "Tweet with the title and URL of this page", true);
+
+key.setGlobalKey(["C-c", "t", "r"],
+    function (ev, arg) {
+        ext.exec("twitter-client-display-timeline", arg);
+    }, "Display your timeline", true);
+
+
+key.setViewKey(['[', '['], function(ev, arg) {
+    ext.exec("previous-page", arg, ev);
+}, "Previous page");
+
+key.setViewKey([']', ']'], function(ev, arg) {
+    ext.exec("next-page", arg, ev);
+}, "Next page");
+
+key.setViewKey('M-<down>', function(ev, arg) {
+    var backForwardMenu = document.getElementById("backForwardMenu");
+    backForwardMenu.openPopupAtScreen(document.width / 2, document.height / 2, true);
+}, 'Show page history menu');
+
+//paste and go
+key.setGlobalKey(["C-c", "p"], function(ev, arg) {
+    ext.exec("paste-and-go", arg, ev);
+}, "Paste and Go");
+
+//paste to tab and go
+key.setGlobalKey(["C-c", "p"], function(ev, arg) {
+    ext.exec("paste-to-tab-and-go", arg, ev);
+}, "Paste to new tab and Go");
+
+key.setViewKey(['C-c', 'q'], function(ev, arg) {
+    ext.exec("search-selection", arg, ev);
+}, "Search the selection with current default engine");
+
+
 
