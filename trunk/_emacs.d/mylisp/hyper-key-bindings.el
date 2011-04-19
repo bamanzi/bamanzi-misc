97caa75a1bf525a90a6deec2417f8b451125be2b
bamanzi@gmail.com <bamanzi@gmail.com@5d397f6c-e14f-10ea-c86a-4de507d69060>
2011-04-19 13:26:22 +0000

    * move window-related command to new file super-key-cmds.el * windswap regards window-dedicated-p

--- trunk/_emacs.d/mylisp/hyper-key-bindings.el
+++ trunk/_emacs.d/mylisp/hyper-key-bindings.el
@@ -95,12 +95,6 @@
 
 (load "folding" t)		;; complementary for outline/hideshow, user-definable mark-based folding
 (load "fold-dwim" t)		;; uniformed front-end for outline/hideshow/folding
-
-(load "tabbar" t)
-(load "imenu-tree" t)
-(load "sr-speedbar" t)
-(load "ide-skel" t)  ;; not recommended, it has some bugs
-
 ;;}}}
 
 (defun hkb--symbol-selected-or-current ()
@@ -233,127 +227,7 @@
 
 ;;}}}
 
-;;{{{ windows
 
-;; sizing
-(global-set-key (kbd "<s-up>") 'enlarge-window)
-(global-set-key (kbd "<s-down>") 'shrink-window)
-(global-set-key (kbd "<s-left>") 'shrink-window-horizontally)
-(global-set-key (kbd "<s-right>") 'enlarge-window-horizontally)
-
-(global-set-key (kbd "<S-s-up>") '(lambda ()
-				   (interactive)
-				   (enlarge-window 3)))
-(global-set-key (kbd"<S-s-down>") '(lambda ()
-				      (interactive)
-				      (shrink-window 3)))
-(global-set-key (kbd "<S-s-left>") '(lambda ()
-				      (interactive)
-				      (shrink-window-horizontally 5)))
-(global-set-key (kbd "<S-s-right>") '(lambda ()
-				       (interactive)
-				       (enlarge-window-horizontally 5)))
-
-(global-set-key (kbd "<s-=>") '(lambda ()
-				 (interactive)
-				 (enlarge-window 1)
-				 (enlarge-window-horizontally 1)))
-(global-set-key (kbd "<s-->") '(lambda ()
-				 (interactive)
-				 (shrink-window 1)
-				 (shrink-window-horizontally 1)))
-(global-set-key (kbd "<s-+>") '(lambda ()
-				 (interactive)
-				 (enlarge-window 3)
-				 (enlarge-window-horizontally 5)))
-(global-set-key (kbd "<s-_>") '(lambda ()
-				 (interactive)
-				 (shrink-window 3)
-				 (shrink-window-horizontally 5)))
-
-;; motion between windows
-(windmove-default-keybindings 'super)
-
-(global-set-key (kbd "<s-tab>") 'other-window)
-(global-set-key (kbd "<S-s-tab>") '(lambda ()
-				     (interactive)
-				     (other-window -1)))
-
-;; move buffer across windows
-;; https://github.com/banister/window-rotate-for-emacs
-(defun rotate-windows-helper(x d)
-  (if (equal (cdr x) nil) (set-window-buffer (car x) d)
-    (set-window-buffer (car x) (window-buffer (cadr x))) (rotate-windows-helper (cdr x) d)))
- 
-(defun rotate-windows ()
-  (interactive)
-  (rotate-windows-helper (window-list) (window-buffer (car (window-list))))
-  (select-window (car (last (window-list)))))
-
-(global-set-key (kbd "<s-backspace>") 'rotate-windows)
-;;TODO: backward-rotate-windows
-
-
-;; modified from windmove-do-window-select
-(defun windmove-do-swap-window (dir &optional arg window)
-  "Move the buffer to the window at direction DIR.
-DIR, ARG, and WINDOW are handled as by `windmove-other-window-loc'.
-If no window is at direction DIR, an error is signaled."
-  (let ((other-window (windmove-find-other-window dir arg window)))
-    (cond ((null other-window)
-           (error "No window %s from selected window" dir))
-          ((and (window-minibuffer-p other-window)
-                (not (minibuffer-window-active-p other-window)))
-           (error "Minibuffer is inactive"))
-          (t
-           (let ( (old-buffer (window-buffer window)) )
-	     (set-window-buffer window (window-buffer other-window))
-	     (set-window-buffer other-window old-buffer)
-	     (select-window other-window))))))
-
-(defun hsb-swap-buffer-up (&optional arg)
-  (interactive "P")
-  (windmove-do-swap-window 'up arg))
-
-(defun hsb-swap-buffer-down (&optional arg)
-  (interactive "P")
-  (windmove-do-swap-window 'down arg))
-
-(defun hsb-swap-buffer-left (&optional arg)
-  (interactive "P")
-  (windmove-do-swap-window 'left arg))
-
-(defun hsb-swap-buffer-right (&optional arg)
-  (interactive "P")
-  (windmove-do-swap-window 'right arg))
-
-(global-set-key (kbd "<C-s-up>") 'hsb-swap-buffer-up)
-(global-set-key (kbd "<C-s-down>") 'hsb-swap-buffer-down)
-(global-set-key (kbd "<C-s-left>") 'hsb-swap-buffer-left)
-(global-set-key (kbd "<C-s-right>") 'hsb-swap-buffer-right)
-
-
-;;tabs	     
-;; (if (featurep 'tabbar)
-;;     (progn
-;;       (global-set-key (kbd "<C-tab>")   'tabbar-forward-tab)
-;;       (global-set-key (kbd "<C-s-tab>") 'tabbar-backward-tab)))
-
-;;
-;; some special windows
-(if (featurep 'imenu-tree)
-    (global-set-key (kbd "s-I") 'imenu-tree))
-
-(if (featurep 'ide-skel)
-    (progn
-      (global-set-key (kbd "s-B") 'ide-skel-toggle-bottom-view-window)
-      (global-set-key (kbd "s-R") 'ide-skel-toggle-right-view-window)
-      (global-set-key (kbd "s-L") 'ide-skel-toggle-left-view-window)))
-
-(global-set-key (kbd "s-s") 'speedbar)
-(if (featurep 'sr-speedbar)
-    (global-set-key (kbd "s-S") 'sr-speedbar-toggle))
-;;}}}
 
 ;;{{{ misc
 ;; bookmarks (within buffer)
