ecf0a7a4be16e6e484b6b2eca5c2600fa0c23787
bamanzi@gmail.com <bamanzi@gmail.com@5d397f6c-e14f-10ea-c86a-4de507d69060>
2011-05-15 09:35:36 +0000

    * new feature: call fortune/cowsay when Emacs became idle * add some comment on usage

--- trunk/_emacs.d/lisp/cowsay-fortune.el
+++ trunk/_emacs.d/lisp/cowsay-fortune.el
@@ -1,22 +1,51 @@
 ;;; cowsay-fortune.el -- insert a fortune cookie with external fortune/cowsay program
 
 
-;;; Bamanzi <bamanzi@gmail.com>
+;; Copyright (C) 2010-2011 BaManzi
+
+;; Author: Bamanzi <bamanzi@gmail.com>
+;; Version: 0.1.3
+;; Keywords: fun, fortune, games
+;; URL: http://bamanzi-misc.googlecode.com
+
+;; This file is NOT part of Emacs.
+
 
 ;;; Commentary:
 ;; You must have `fortune' program & data files installed.
+;; ( http://packages.debian.org/wheezy/fortune-mod )
+
 ;;
 ;; M-x fortune  	;; show a fortune cookie
 ;; M-x insert-fortune   ;; insert a fortune cookie into current buffer
 ;;
+;; M-x idle-fortune-start
+;;                     ;; Start a timer so that a fortune cookie would be
+;;                     ;; shown each time Emacs became idle
+;; M-x idle-fortune-stop
+;;                     ;; Stop the timer started by `idle-fortune-start'
 ;;
 ;; You must have `cowsay' program installed to use the following commands:
+;; (cowsay: http://www.nog.net/~tony/warez/cowsay.shtml)
 ;;
-;; M-x cowsay-quote-region      ;; quote the region with a cowsay figure
-;; M-x insert-cowsay-fortune    ;; insert a fortune cookie, quoted with a cowsay figure
+;; M-x cowsay-quote-region
+;;                     ;; quote the region with a cowsay figure
+;; M-x insert-cowsay-fortune
+;;                     ;; insert a fortune cookie, quoted with a cowsay figure
 ;; M-x insert-random-cowsay-fortune
-;; 				;; similar to `insert-cowsay-fortune', but use random cowsay figure
+;;                     ;; similar to `insert-cowsay-fortune', but use random
+;;                     ;; cowsay figure
+;; M-x ido-insert-cowsay-fortune
+;;                     ;; similar to `insert-cowsay-fortune', but you can choose
+;;                     ;; which cowsay figure and which fortune database to use.
 ;;
+;; Note: For Windows user, refer `init-cygwin-cowsay-fortune'. You may need
+;; add it into your ~/.emacs.
+;;
+;; Hint: If you don't want to install `fortune', and have a good internet connection,
+;; you can use `run-with-idle-time' to run (browse-url "http://fortunemod.com/")
+
+
 ;; Sample output of `insert-cowsay-fortune':
 ;; _________________________________________
 ;; / "What do you mean with disappear - it's \
@@ -122,6 +151,45 @@ This one would use `ido' to let user choose the cowfile and the fortune cookie f
                                               )) )                                              
      (insert-string (cowsay-fortune cowfile fortune-file))))
 
+;;---
+(defcustom idle-fortune-use-cowsay t
+  "Whether to use `cowsay' in `idle-fortune-show'.
+
+If you have no `cowsay', you can turn it off."
+  :type 'boolean)
+
+(defun idle-fortune-show ()
+  (interactive)
+  (let ( (win (selected-window)) )
+    (if (fboundp 'windmove-find-other-window)  ;; use `windmove' to find the bottom window
+        (while (windmove-find-other-window 'down nil win)
+          (setq win (windmove-find-other-window 'down nil win))))
+    (when win
+      (with-selected-window win
+        (when (get-buffer-create "*fortune*")
+          (with-current-buffer "*fortune*"
+            (kill-region (point-min) (point-max))
+            (insert-string "--\n")
+            (if idle-fortune-use-cowsay
+                (insert-cowsay-fortune)
+              (insert-fortune "")))
+          (switch-to-buffer "*fortune*")
+          (beginning-of-buffer))))
+      ))
+
+(defun idle-fortune-start ()
+  "Start to show a random fortune cookie when Emacs is idle for 60 second."
+  (interactive)
+  (setq idle-fortune-timer (run-with-idle-timer 60 t 'idle-fortune-show)))
+
+(defun idle-fortune-stop ()
+  "Stop the timer started by `idle-fortune-start'."
+  (interactive)
+  (cancel-timer idle-fortune-timer))
+        
+
+;;---  
+
 (defun init-cygwin-cowsay-fortune (cygwin-path)
   "Init `fortune-prg', `fortune-data-dir', `cowsay-prg' and `cowsay-data-dir' with Cygwin."  
   (setq fortune-prg	(concat cygwin-path "/bin/fortune"))
@@ -130,8 +198,8 @@ This one would use `ido' to let user choose the cowfile and the fortune cookie f
   (setq cowsay-prg	(concat cygwin-path "/bin/env LC_ALL= LANG= /bin/perl /usr/local/bin/cowsay"))
   (setq cowsay-data-dir	(concat cygwin-path "/usr/share/cowsay")))
 
-(if (eq system-type 'windows-nt)
-    (init-cygwin-cowsay-fortune))
+;; (if (eq system-type 'windows-nt)
+;;     (init-cygwin-cowsay-fortune "e:/cygwin"))
  
   
 
