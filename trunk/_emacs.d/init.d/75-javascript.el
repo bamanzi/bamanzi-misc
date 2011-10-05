ab484f2bc1ee1708316024e8d4573b9243a8fea4
bamanzi <bamanzi@5d397f6c-e14f-10ea-c86a-4de507d69060>
2011-10-05 13:32:11 +0000

    New file for javascript

---
+++ trunk/_emacs.d/init.d/75-javascript.el
@@ -0,0 +1,45 @@
+;;;_. major modes
+
+;; js2-mode has better imenu support for extjs style object method
+(autoload 'js2-mode      "js2-mode" nil t)
+
+(autoload 'espresso-mode "espresso" nil t)
+
+(if (>= emacs-major-version 24)
+    (add-to-list 'auto-mode-alist '("\\.js\\'" . espresso-mode)))
+
+;;;_. code folding
+(defun js-mode-init-fold ()
+  (if (require 'hideshowvis nil t)
+      (hideshowvis-enable)
+    (hs-minor-mode t))
+  
+  ;;TODO:
+;;  (setq outline-regexp "...")
+  )
+
+;;;_. json-pretty-print
+
+;;;_. code completion
+;;;_.. autocomplete + semantic
+(defun js-mode-init-auto-complete ()
+  (add-to-list 'ac-modes 'espresso-mode)
+  
+  )
+;;;_. lint
+;;;_.. v8 (http://koansys.com/tech/flymake-mode-for-emacs-javascript-v8-edition)
+(defun js-lint-with-v8 ()
+  (let ( (compile-command (concat "jslint " buffer-file-name)) )
+    (call-interactively 'compile)))
+
+(eval-after-load "espresso"
+  '(define-key espresso-mode (kbd "<M-f9>")  'js-lint-with-v8))
+
+
+;;;_. misc
+;;;_.. which-func-mode
+(add-to-list 'which-func-modes 'js-mode)
+(add-to-list 'which-func-modes 'ecmacript-mode)
+(add-to-list 'which-func-modes 'js2-mode)
+(add-to-list 'which-func-modes 'espresso-mode)
+
