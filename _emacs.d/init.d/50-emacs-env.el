3f687af43b90a81dc4f1e2726b114796c9d95988
bamanzi <bamanzi@5d397f6c-e14f-10ea-c86a-4de507d69060>
2011-10-05 13:32:42 +0000

    various improvments

--- _emacs.d/init.d/50-emacs-env.el
+++ _emacs.d/init.d/50-emacs-env.el
@@ -1,6 +1,7 @@
 
 ;;;_. options
 ;;(setq custom-unlispify-tag-names nil)
+
 (global-set-key (kbd "<C-f10> g") 'customize-group)
 (global-set-key (kbd "<C-f10> v") 'customize-variable)
 (global-set-key (kbd "<C-f10> f") 'customize-face)
@@ -44,8 +45,39 @@
       (ad-set-args 1 (cons trace (ad-get-args 1))) ))
 
 
-   (ad-disable-advice 'message 'before 'who-said-that)
-   (ad-update 'message)
+(ad-disable-advice 'message 'before 'who-said-that)
+(ad-update 'message)
+
+  (defadvice error (before who-said-that activate)
+    "Find out who said that thing. and say so."
+    (let ((trace nil) (n 1) (frame nil))
+      (while (setq frame (backtrace-frame n))
+        (setq n     (1+ n) 
+              trace (cons (cadr frame) trace)) )
+      (ad-set-arg 0 (concat "<<%S>>:\n" (ad-get-arg 0)))
+      (ad-set-args 1 (cons trace (ad-get-args 1))) ))
+
+
+(ad-disable-advice 'error 'before 'who-said-that)
+(ad-update 'error)
+
+(defun bmz/toggle-debug-on-error ()
+  (interactive)
+  (if debug-on-error
+      (progn ;;turn if off
+        (setq debug-on-error nil)
+        (ad-deactivate 'error)
+        (ad-deactivate 'message))
+    (progn  ;;turn it on
+      (setq debug-on-error t)
+      (ad-activate 'error)
+      (ad-activate 'message)))
+  (message "Debug on Error %s globally" (if debug-on-error
+                                            "enabled"
+                                          "disabled")))
+
+(global-set-key (kbd "<C-f10> d") 'bmz/toggle-debug-on-error)
+
 
 
 ;;;_. "utils"
@@ -84,10 +116,14 @@ Otherwise, call the original `dired-jump'."
 
 
 (setq ediff-split-window-function 'split-window-horizontally)
-;; split the window depending on the frame width:
-(setq ediff-split-window-function (if (> (frame-width) 150)
-                                      'split-window-horizontally
-                                    'split-window-vertically))
+;; split the window depending on the frame width
+(setq ediff-split-window-function (lambda (&optional arg)
+                                    (if (> (frame-width) 150)
+                                        (split-window-horizontally arg)
+                                      (split-window-vertically arg))))
+
+
+
 
 
 
