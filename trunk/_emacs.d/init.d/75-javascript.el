4850f7ab15cc51055eee9b7236a3e88ec3fb36a0
bamanzi@gmail.com <bamanzi@gmail.com@5d397f6c-e14f-10ea-c86a-4de507d69060>
2011-11-20 13:12:53 +0000

    some minor changes

--- trunk/_emacs.d/init.d/75-javascript.el
+++ trunk/_emacs.d/init.d/75-javascript.el
@@ -1,4 +1,5 @@
-;;;_. major modes
+;;* JavaScript
+;;** major modes
 
 ;; js2-mode has better imenu support for extjs style object method
 (autoload 'js2-mode      "js2-mode" nil t)
@@ -8,7 +9,7 @@
 (if (>= emacs-major-version 24)
     (add-to-list 'auto-mode-alist '("\\.js\\'" . espresso-mode)))
 
-;;;_. code folding
+;;** code folding
 (defun js-mode-init-fold ()
   (if (require 'hideshowvis nil t)
       (hideshowvis-enable)
@@ -18,14 +19,9 @@
 ;;  (setq outline-regexp "...")
   )
 
-;;;_. json-pretty-print
 
-(defun json-pretty-print (begin end)
-  (interactive "r")
-  (shell-command-on-region begin end "python -mjson.tool" nil 'replace))
-
-;;;_. code completion
-;;;_.. autocomplete + semantic
+;;**  code completion
+;; autocomplete + semantic
 (defun js-mode-init-auto-complete ()
   (add-to-list 'ac-modes 'espresso-mode)
   
@@ -58,16 +54,21 @@
 ;;*** rhino + jslint
 ;;.....
 
-
-;;*** b
+;;*** keybindings
 (eval-after-load "espresso"
   '(define-key espresso-mode (kbd "<M-f9>")  'jslint-with-node))
 
 
-;;;_. misc
-;;;_.. which-func-mode
+
+;;** misc
+;;*** which-func-mode
 (add-to-list 'which-func-modes 'js-mode)
 (add-to-list 'which-func-modes 'ecmacript-mode)
 (add-to-list 'which-func-modes 'js2-mode)
 (add-to-list 'which-func-modes 'espresso-mode)
 
+;;*** json-pretty-print
+
+(defun json-pretty-print (begin end)
+  (interactive "r")
+  (shell-command-on-region begin end "python -mjson.tool" nil 'replace))
