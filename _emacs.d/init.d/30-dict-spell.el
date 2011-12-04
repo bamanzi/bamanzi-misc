4113179c9b4a3ac3f90e1379b7ada50c039d365f
bamanzi@gmail.com <bamanzi@gmail.com@5d397f6c-e14f-10ea-c86a-4de507d69060>
2011-12-04 13:33:51 +0000

    New file

---
+++ _emacs.d/init.d/30-dict-spell.el
@@ -0,0 +1,32 @@
+
+;;** DICT protocol
+;;*** dictionary.el
+;;    (setq dictionary-server "localhost")
+(autoload 'dictionary-search "dictionary" "Ask for a word and search it in all dictionaries" t)
+(autoload 'dictionary-match-words "dictionary" "Ask for a word and search all matching words in the dictionaries" t)
+
+;;*** dictem.el
+;; external utility `dict' client needed
+(setq dictem-server "localhost")
+(autoload 'dictem-run-search "dictem" nil t)
+(autoload 'dictem-run-match  "dictem" nil t) 
+
+;;*** dict.el (external program `dict' needed)
+;;;  (NOTE: it's hard to use it on windows)
+(setq dict-servers '("localhost" "dict.org"))
+;;(setq dict-enable-key-bindings t)
+;;(setq dict-databases '("gcide" "pydict"))
+(autoload 'dict "dict" "Lookup a WORD on dict.org." t)
+
+
+;;** stardict
+
+;;*** sdcv-mode.el
+(autoload 'sdcv-search "sdcv-mode" nil t)
+
+;;*** sdcv.el
+;;FIXME: problem
+
+
+;;** spell
+;;
