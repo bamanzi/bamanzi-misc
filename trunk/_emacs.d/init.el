4f9f471eb12ba491af1c0357f8d90d9efcd38311
bamanzi@gmail.com <bamanzi@gmail.com@5d397f6c-e14f-10ea-c86a-4de507d69060>
2011-12-18 13:10:28 +0000

    make idle-require faster

--- trunk/_emacs.d/init.el
+++ trunk/_emacs.d/init.el
@@ -29,9 +29,6 @@
 ;;}}}
 
 
-
-
-
 ;;{{{ generate autoload and load them
 (defun generate-dir-autoload-and-load (top-dir)
   (let ( (generated-autoload-file "~/.emacs.d/autoloads.el") )
@@ -45,36 +42,80 @@
     (load-file generated-autoload-file)))
 
 
-;;;_ misc
+
+;;** main
+(setq idle-require-idle-delay 5)
+(setq idle-require-load-break 2)
+
 (unless (load "idle-require" t)
   ;; fail-safe for `idle-quire'
   (defun idle-require (feature &optional file noerror)
     (require feature)))
 ;; (setq idle-require-symbols '(cedet nxml-mode)) ;; <- Specify packages here.
-;;(idle-require 'foobar)
+
 
 (mapc  '(lambda (file)
-	  (ignore-errors
-	    (load file)))
-       (directory-files "~/.emacs.d/init.d/" t "[0-9[0-9].*\.el$"))
+          (unless ;;(ignore-errors        
+                    (load file);;)
+            (message "Failed to load %s." file)))          
+       (directory-files "~/.emacs.d/init.d/" t "^[0-9[0-9].*\.el$"))
 
-;; (load "~/.emacs.d/init.d/10-init.el")
+(when nil
+  ;; for debugging initscripts
+  (progn
+    (insert-string "\n  (progn\n")
+    (mapc '(lambda (file)
+             (insert-string (format "   (load \"%s\")\n" file)))
+          (directory-files "~/.emacs.d/init.d/" t "[0-9[0-9].*\.el$"))
+      (insert-string ")  \n"))  ;;put cursor on next line, and press C-x C-e
+    
+  (progn
+   (load "/home/bamanzi/.emacs.d/init.d/00-init.el")
+   (load "/home/bamanzi/.emacs.d/init.d/10-emacs-env.el")
+   (load "/home/bamanzi/.emacs.d/init.d/10-essential.el")
+   (load "/home/bamanzi/.emacs.d/init.d/20-buffers.el")
+   (load "/home/bamanzi/.emacs.d/init.d/20-emacs-env.el")
+   (load "/home/bamanzi/.emacs.d/init.d/20-files.el")
+   (load "/home/bamanzi/.emacs.d/init.d/20-windows.el")
+   (load "/home/bamanzi/.emacs.d/init.d/25-minibuffer.el")
+   (load "/home/bamanzi/.emacs.d/init.d/25-tabbar.el")
+   (load "/home/bamanzi/.emacs.d/init.d/25-win-fns.el")
+   (load "/home/bamanzi/.emacs.d/init.d/29-env-misc.el")
+   (load "/home/bamanzi/.emacs.d/init.d/30-shell.el")
+   (load "/home/bamanzi/.emacs.d/init.d/40-completion.el")
+   (load "/home/bamanzi/.emacs.d/init.d/40-edit-basic.el")
+   (load "/home/bamanzi/.emacs.d/init.d/40-fold-nav.el")
+   (load "/home/bamanzi/.emacs.d/init.d/40-highlights.el")
+   (load "/home/bamanzi/.emacs.d/init.d/40-symbol-fns.el")
+   (load "/home/bamanzi/.emacs.d/init.d/49-edit-misc.el")
+   (load "/home/bamanzi/.emacs.d/init.d/50-cua-keys.el")
+   (load "/home/bamanzi/.emacs.d/init.d/50-vi-emu.el")
+   (load "/home/bamanzi/.emacs.d/init.d/50-vi-keys.el")
+   (load "/home/bamanzi/.emacs.d/init.d/59-misc-keys.el")
+   (load "/home/bamanzi/.emacs.d/init.d/59-mouse.el")
+   (load "/home/bamanzi/.emacs.d/init.d/60-prog-basic.el")
+   (load "/home/bamanzi/.emacs.d/init.d/65-cedet-ecb.el")
+   (load "/home/bamanzi/.emacs.d/init.d/70-file-assoc.el")
+   (load "/home/bamanzi/.emacs.d/init.d/75-delphi.el")
+   (load "/home/bamanzi/.emacs.d/init.d/75-elisp.el")
+   (load "/home/bamanzi/.emacs.d/init.d/75-javascript.el")
+   (load "/home/bamanzi/.emacs.d/init.d/75-org-mode.el")
+   (load "/home/bamanzi/.emacs.d/init.d/75-python.el")
+   (load "/home/bamanzi/.emacs.d/init.d/75-sh-script.el")
+   (load "/home/bamanzi/.emacs.d/init.d/75-xml.el")
+   (load "/home/bamanzi/.emacs.d/init.d/79-autohotkey.el")
+   (load "/home/bamanzi/.emacs.d/init.d/80-ports.el")
+   (load "/home/bamanzi/.emacs.d/init.d/90-foobar.el")
+   (load "/home/bamanzi/.emacs.d/init.d/95-one-key.el")
+   (load "/home/bamanzi/.emacs.d/init.d/99-temp.el")
+  )
+ 
+  )
 
-;; (load "~/.emacs.d/init.d/20-symbol-fns" t)
-;; (load "~/.emacs.d/init.d/20-win-fns" t)
-
-;; (load "~/.emacs.d/init.d/50-misc" t)
-
-;; (load "~/.emacs.d/init.d/60-cua-keys" t)
-;; (load "~/.emacs.d/init.d/60-vi-keys" t)
 
 
-;; (load "~/.emacs.d/init.d/80-mouse" t)
 
-;; (load "~/.emacs.d/init.d/95-one-key" t)
-
-
-(if (fboundp 'idle-require-mode)
+(when (fboundp 'idle-require-mode)
   (idle-require-mode t))
 
 ;;restore stub
