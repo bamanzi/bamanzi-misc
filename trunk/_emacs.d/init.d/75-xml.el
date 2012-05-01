26835f52f22022db51447cebc631153a98f748c6
bamanzi@gmail.com <bamanzi@gmail.com@5d397f6c-e14f-10ea-c86a-4de507d69060>
2012-05-01 14:38:25 +0000

    75-xml.el: add bf-pretty-print-xml-region 75-org-mode.el: set org-src-fontify-natively  org-return-follows-link

--- trunk/_emacs.d/init.d/75-xml.el
+++ trunk/_emacs.d/init.d/75-xml.el
@@ -2,6 +2,21 @@
 ;;;_. for `sgml-pretty-print'
 (autoload 'sgml-pretty-print "sgml-mode" "Simple-minded pretty printer for SGML" t)
 
+;;stolen from http://blog.bookworm.at/2007/03/pretty-print-xml-with-emacs.html
+(defun bf-pretty-print-xml-region (begin end)
+  "Pretty format XML markup in region. You need to have nxml-mode
+http://www.emacswiki.org/cgi-bin/wiki/NxmlMode installed to do
+this. The function inserts linebreaks to separate tags that have
+nothing but whitespace between them. It then indents the markup
+by using nxml's indentation rules."
+  (interactive "r")
+  (save-excursion
+    (nxml-mode)
+    (goto-char begin)
+    (while (search-forward-regexp "\>[ \\t]*\<" nil t) 
+      (backward-char) (insert "\n") (setq end (1+ end)))
+    (indent-region begin end))
+  (message "Ah, much better!"))
 
 ;;;_.  Showing XPath in modeline
 ;; http://www.emacswiki.org/emacs/NxmlMode
